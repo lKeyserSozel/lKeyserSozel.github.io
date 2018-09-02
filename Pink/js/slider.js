30eb667af25ba4ddaf92232ae449be2d054373fa
lKeyserSozel <arman152347@gmail.com>
2018-09-02 16:04:43 +0300

    features + review

--- Pink/js/slider.js
+++ Pink/js/slider.js
@@ -1,14 +1,14 @@
-var reviewBtn = document.querySelectorAll('.review__btn-wrapper label');
-var reviewSlider = document.querySelector('.review__slider');
+// var reviewBtn = document.querySelectorAll('.review__btn-wrapper label');
+// var reviewSlider = document.querySelector('.review__slider');
 var priceBtn = document.querySelectorAll('.price__btn-wrapper label');
 var priceSlider = document.querySelector('.price__table');
 
 
-reviewBtn.forEach(function (el, i) {
-	el.addEventListener('click', function () {
-		reviewSlider.style.marginLeft = i * -300 + 'px';
-	})
-});
+// reviewBtn.forEach(function (el, i) {
+// 	el.addEventListener('click', function () {
+// 		reviewSlider.style.marginLeft = i * -300 + 'px';
+// 	})
+// });
 
 
 priceBtn.forEach(function (el, i) {
@@ -25,7 +25,7 @@ var window_size = window.matchMedia('(min-width: 768px)');
 if (window.matchMedia('(min-width: 768px)').matches) {
 	reviewBtn.forEach(function (el, i) {
 		el.addEventListener('click', function () {
-			reviewSlider.style.marginLeft = i * -655 + 'px';
+			reviewSlider.style.marginLeft = i * -667 + 'px';
 		})
 	});
 }
