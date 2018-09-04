6048fb27e4e34677b8a192b936f2408c5184ba76
lKeyserSozel <arman152347@gmail.com>
2018-09-04 19:44:25 +0300

    photo (gallery)

--- Pink/js/common.js
+++ Pink/js/common.js
@@ -2,7 +2,7 @@ var navList = document.querySelector('.main-nav__list');
 var navClose = document.querySelector('.main-nav__close-btn');
 var navBurger = document.querySelector('.main-nav__burger');
 
-document.body.classList.remove('no-js');
+// document.body.classList.remove('no-js');
 
 if (navList.classList.contains('main-nav__list--opened')) {
 	navList.classList.remove('main-nav__list--opened');
