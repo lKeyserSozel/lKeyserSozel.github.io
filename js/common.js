5665f589d333fb3d3d18e8732f8b2e699748a451
lKeyserSozel <arman152347@gmail.com>
2018-12-15 20:22:41 +0200

    Change weather (sunny)

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
-var weather = document.querySelector(".weather");
+(function onWeather() {
+	var weather = document.querySelector(".weather");
+	var weatherCloud = weather.querySelector(".weather__cloud");
 
-setTimeout( function() {
-	weather.classList.add("weather--on");
-}, 1000);
+	setTimeout(function () {
+		weather.classList.add("weather--on");
+	}, 1000);
+
+	weather.addEventListener('click', hiddenCloud);
+
+	function hiddenCloud() {
+		weatherCloud.classList.toggle("weather__cloud--closed");
+		document.body.classList.toggle("sunny");
+	}
+	
+})();
+
