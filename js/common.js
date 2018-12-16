873453c2eef162658a6666aa9f3b33173b5a9da8
lKeyserSozel <arman152347@gmail.com>
2018-12-16 23:14:12 +0200

    Car add

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 (function onWeather() {
 	var weather = document.querySelector(".weather");
 	var weatherCloud = weather.querySelector(".weather__cloud");
+	var car = document.querySelector(".car");
 
 	setTimeout(function () {
 		weather.classList.add("weather--on");
@@ -10,6 +11,7 @@
 
 	function hiddenCloud() {
 		weatherCloud.classList.toggle("weather__cloud--closed");
+		car.classList.add("car--move");
 		setTimeout(function () {
 			document.body.classList.toggle("sunny");
 		}, 500);
