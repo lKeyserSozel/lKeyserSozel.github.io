f948bc935c39433566160975fe0ae7ad4fbfb3c9
lKeyserSozel <arman152347@gmail.com>
2021-01-26 17:59:47 +0200

    Add Melik

---
+++ Melik/index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/animate.min.css">
+    <link rel="stylesheet" href="assets/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/meanmenu.css">
+    <link rel="stylesheet" href="assets/flaticon.css">
+    <link rel="stylesheet" href="assets/magnific-popup.min.css">
+    <link rel="stylesheet" href="assets/odometer.min.css">
+    <link rel="stylesheet" href="assets/twentytwenty.min.css">
+    <link rel="stylesheet" href="assets/slick.min.css">
+    <link rel="stylesheet" href="assets/nice-select.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/responsive.css">
+    <title>Dental</title>
+    <style type="text/css" id="#jarallax-clip-0">
+        #jarallax-container-0 {
+            clip: rect(0 1440px 536px 0);
+            clip: rect(0, 1440px, 536px, 0);
+        }</style>
+</head>
+<body>
+
+
+<div class="navbar-area navbar-style-two is-sticky">
+    <div class="klev-responsive-nav">
+        <div class="container">
+            <div class="klev-responsive-menu">
+                <div class="logo">
+                    <a href="/">
+                        <img src="assets/logo.png" alt="logo">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="klev-nav">
+        <div class="container">
+            <nav class="navbar navbar-expand-md navbar-light">
+                <a class="navbar-brand" href="/">
+                    <img src="assets/logo.png" alt="logo">
+                </a>
+                <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent" style="display: block;">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item">
+                            <a href="#contacts" class="nav-link">
+                                Контакты
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+
+
+<div class="home-slides">
+
+    <div class="main-banner banner-section item-bg4">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="main-banner-content">
+                                    <span class="sub-title animated fadeInLeft"
+                                          style="opacity: 1;">Keeping Teeth Clean</span>
+                        <h1 style="opacity: 1;" class="animated fadeInLeft">Klev Health, Keeping Care Close
+                            to Home</h1>
+                        <p style="opacity: 1;" class="animated fadeInUp">Lorem ipsum dolor sit amet,
+                            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna Quis ipsumpsum.</p>
+                        <div class="btn-box">
+                            <a href="https://templates.envytheme.com/klev/default/index-2.html#"
+                               class="btn btn-primary animated fadeIn" style="opacity: 1;">Contact Us <i
+                                        class="flaticon-next"></i></a>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<section class="featured-services-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="featured-services-box">
+                    <div class="icon">
+                        <i class="flaticon-doctor"></i>
+                    </div>
+                    <h3>Professional Staff</h3>
+
+                    <div class="shape">
+                        <img src="assets/line-shape.png" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="featured-services-box">
+                    <div class="icon">
+                        <i class="flaticon-laboratory"></i>
+                    </div>
+                    <h3>Oral Surgery</h3>
+
+                    <div class="shape">
+                        <img src="assets/line-shape.png" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="featured-services-box">
+                    <div class="icon">
+                        <i class="flaticon-baby-boy"></i>
+                    </div>
+                    <h3>Kids Dentist</h3>
+
+                    <div class="shape">
+                        <img src="assets/line-shape.png" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="featured-services-box">
+                    <div class="icon">
+                        <i class="flaticon-tooth-3"></i>
+                    </div>
+                    <h3>Crowns Lab</h3>
+
+                    <div class="shape">
+                        <img src="assets/line-shape.png" alt="image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="about-area pt-100">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-12">
+                <div class="about-image">
+                    <img src="assets/about-img1.jpg" alt="image">
+                    <div class="text">
+                        <p>
+                            <span>10</span>
+                            Лет Опыта
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="about-content">
+                    <span class="sub-title">Кто мы</span>
+                    <h2>Имплантологический центр</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                    <ul class="features-list">
+                        <li>
+                            <span><i class="fas fa-check"></i> Complete Crown</span>
+                        </li>
+                        <li>
+                            <span><i class="fas fa-check"></i> Cosmetic Filling</span>
+                        </li>
+                        <li>
+                            <span><i class="fas fa-check"></i> Dental Implants</span>
+                        </li>
+                        <li>
+                            <span><i class="fas fa-check"></i> Dental Crown</span>
+                        </li>
+                        <li>
+                            <span><i class="fas fa-check"></i> Dental X-Ray</span>
+                        </li>
+                        <li>
+                            <span><i class="fas fa-check"></i> Root Canal</span>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="services-area ptb-100">
+    <div class="container">
+        <div class="section-title text-left">
+            <span class="sub-title">Наши услуги</span>
+            <h2>What We Offer for You to Our Patients to solve Cure</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.</p>
+            <a href="https://templates.envytheme.com/klev/default/index-2.html#" class="btn btn-primary">More Services
+                <i class="flaticon-next-1"></i></a>
+        </div>
+        <div class="row m-0">
+            <div class="col-lg-4 col-md-6 col-sm-6 p-0">
+                <div class="single-services-box">
+                    <h3><i class="flaticon-dental-implant"></i>Импланты</h3>
+
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 p-0">
+                <div class="single-services-box">
+                    <h3><i class="flaticon-tooth color-043d72"></i> Dental X-Ray <br>Treatment</h3>
+
+
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 p-0">
+                <div class="single-services-box">
+                    <h3><i class="flaticon-tooth-1"></i>Эндодонтия<br>Treatment</h3>
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
+
+
+<section class="pricing-area pt-100 pb-70">
+    <div class="container">
+        <div class="section-title">
+            <span class="sub-title">Pricing</span>
+            <h2>Cosmetic Composite White Fillings</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.</p>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-pricing-box">
+                    <div class="pricing-header bg1">
+                        <h3>Basic</h3>
+                    </div>
+                    <ul class="pricing-features-list">
+                        <li><i class="fas fa-check"></i> Complete Denture</li>
+                        <li><i class="fas fa-check"></i> Cosmetic Filling</li>
+                        <li><i class="fas fa-check"></i> Dental Implants</li>
+                        <li><i class="fas fa-check"></i> Dental Crown and Bridge</li>
+                        <li><i class="fas fa-check"></i> Dental X-Ray</li>
+                    </ul>
+                    <div class="price">
+                        <sub>$</sub>179<sub>/mo</sub>
+                    </div>
+                    <div class="book-now-btn">
+                        <a href="https://templates.envytheme.com/klev/default/index-2.html#" class="btn btn-primary">
+                            Choose Plan <i class="flaticon-next"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-pricing-box">
+                    <div class="pricing-header bg2">
+                        <h3>Professional</h3>
+                    </div>
+                    <ul class="pricing-features-list">
+                        <li><i class="fas fa-check"></i> Complete Denture</li>
+                        <li><i class="fas fa-check"></i> Cosmetic Filling</li>
+                        <li><i class="fas fa-check"></i> Dental Implants</li>
+                        <li><i class="fas fa-check"></i> Dental Crown and Bridge</li>
+                        <li><i class="fas fa-check"></i> Dental X-Ray</li>
+                    </ul>
+                    <div class="price">
+                        <sub>$</sub>295<sub>/mo</sub>
+                    </div>
+                    <div class="book-now-btn">
+                        <a href="https://templates.envytheme.com/klev/default/index-2.html#" class="btn btn-primary">
+                            Choose Plan <i class="flaticon-next"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+                <div class="single-pricing-box">
+                    <div class="pricing-header bg3">
+                        <h3>Premium</h3>
+                    </div>
+                    <ul class="pricing-features-list">
+                        <li><i class="fas fa-check"></i> Complete Denture</li>
+                        <li><i class="fas fa-check"></i> Cosmetic Filling</li>
+                        <li><i class="fas fa-check"></i> Dental Implants</li>
+                        <li><i class="fas fa-check"></i> Dental Crown and Bridge</li>
+                        <li><i class="fas fa-check"></i> Dental X-Ray</li>
+                    </ul>
+                    <div class="price">
+                        <sub>$</sub>139<sub>/mo</sub>
+                    </div>
+                    <div class="book-now-btn">
+                        <a href="https://templates.envytheme.com/klev/default/index-2.html#" class="btn btn-primary">
+                            Choose Plan <i class="flaticon-next"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="doctor-area pt-100 extra-pb bg-image">
+    <div class="container">
+        <div class="section-title text-left">
+            <span class="sub-title">Our Doctors</span>
+            <h2>The most qualified doctors Have good knowledge</h2>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="single-doctor-box">
+                    <div class="image">
+                        <img src="assets/dr-photo.jpg" alt="image">
+                    </div>
+                    <div class="content">
+                        <h3>Доктор Маргарян</h3>
+                        <span>Главный врач</span>
+                        <ul class="social">
+                            <li>
+                                <a href="https://facebook.com"><i
+                                            class="fab fa-facebook-f"></i></a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.instagram.com/dr.m.margaryan/"><i
+                                            class="fab fa-instagram"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
+
+
+<section class="cta-area ptb-100 jarallax" data-jarallax="{&quot;speed&quot;: 0.3}" data-jarallax-original-styles="null"
+         style="background-image: none; background-attachment: scroll; background-size: auto;">
+    <div class="container">
+        <div class="cta-content">
+            <div class="icon">
+                <i class="flaticon-business-and-finance"></i>
+            </div>
+            <h2>Emergency Medical Care 24/7</h2>
+            <p>With access to 24 hour emergency assistance, It’s so important you can continue to help others.</p>
+            <a href="https://templates.envytheme.com/klev/default/index-2.html#" class="call-us"><i
+                        class="flaticon-phone-call"></i> 342-875-0564
+            </a>
+        </div>
+    </div>
+    <div id="jarallax-container-0"
+         style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -100;">
+        <div style="background-position: 50% 50%; background-size: 100%; background-repeat: no-repeat; background-image: url(&quot;https://templates.envytheme.com/klev/default/assets/img/cta-bg.jpg&quot;); position: fixed; top: 0px; left: 0px; width: 1440px; height: 810px; overflow: hidden; pointer-events: none; margin-left: 0px; margin-top: -45.5px; visibility: visible; transform: translateY(183px) translateZ(0px);"></div>
+    </div>
+</section>
+
+
+<footer class="footer-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-6">
+                <div class="single-footer-widget">
+                    <div class="logo">
+                        <img src="assets/white-logo.png" alt="image">
+                        <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna
+                            aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra lacus vel
+                            facilisis.</p>
+                    </div>
+                    <ul class="social">
+                        <li>
+                            <a href="https://templates.envytheme.com/klev/default/index-2.html#"><i
+                                        class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://templates.envytheme.com/klev/default/index-2.html#"><i
+                                        class="fab fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-sm-3 offset-md-3">
+                <div class="single-footer-widget">
+                    <h3>Контакты</h3>
+                    <ul class="footer-contact-info">
+                        <li>
+                            <span>Адрес:</span>
+                            27 Division St, New York, NY 1002, USA
+                        </li>
+                        <li>
+                            <span>Email:</span>
+                            <a href="https://templates.envytheme.com/klev/default/index-2.html#">hello@klev.com</a>
+                        </li>
+                        <li>
+                            <span>Телефон:</span>
+                            <a href="https://templates.envytheme.com/klev/default/index-2.html#">+(321) 948 754</a>
+                        </li>
+
+                        <li>
+                            <a href="https://goo.gl/maps/1xz4L8TGSdkBhVmb7" target="_blank">Посмотреть адрес на Карте
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-sm-6 col-md-6">
+                    <p><i class="far fa-copyright"></i> 2021 Copyright. All Rights Reserved</p>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-md-6">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="go-top"><i class="fas fa-chevron-up"></i><i class="fas fa-chevron-up"></i></div>
+<script src="assets/jquery.min.js"></script>
+<script src="assets/popper.min.js"></script>
+<script src="assets/bootstrap.min.js"></script>
+<script src="assets/parallax.min.js"></script>
+<script src="assets/owl.carousel.min.js"></script>
+<script src="assets/slick.min.js"></script>
+<script src="assets/jquery.meanmenu.js"></script>
+<script src="assets/jquery.appear.min.js"></script>
+<script src="assets/odometer.min.js"></script>
+<script src="assets/event.move.min.js"></script>
+<script src="assets/twentytwenty.min.js"></script>
+<script src="assets/jquery.nice-select.min.js"></script>
+<script src="assets/jquery.magnific-popup.min.js"></script>
+<script src="assets/wow.min.js"></script>
+<script src="assets/jquery.ajaxchimp.min.js"></script>
+<script src="assets/form-validator.min.js"></script>
+<script src="assets/contact-form-script.js"></script
+<script src="assets/main.js"></script>
+
+</body>
+</html>
